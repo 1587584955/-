5f0eb02996d36b6fd9ab2f69bd1b23c7a9432bfe
hy <1587584955@qq.com>
2020-05-21 16:17:25 +0800

    实现发送邮件业务，读取properties乱码，用字节流读取即可

--- killmall/src/main/java/seu/hy/killmall/service/EmailReciveService.java
+++ killmall/src/main/java/seu/hy/killmall/service/EmailReciveService.java
@@ -1,15 +1,44 @@
 package seu.hy.killmall.service;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.MailSender;
 import org.springframework.stereotype.Service;
+import seu.hy.killmall.dto.MailDto;
 import seu.hy.killmall.pojo.KillSuccessUserInfo;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
 
 @Service
 public class EmailReciveService {
 
+    private  static Properties pro;
+    static {
+        String filename="application.properties";
+        pro= new Properties();
+        try {
+            pro.load(new InputStreamReader(MailService.class.getClassLoader().getResourceAsStream(filename),"utf-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Autowired
+    private MailService mailService;
+
     @RabbitListener(queues = {"test.kill.item.success.email.queue"},containerFactory = "singleListenerContainer")
     public void consumerEmailMsg(KillSuccessUserInfo info) {
         try {
+            MailDto mailDto=new MailDto();
+            mailDto.setTos(new String[]{info.getEmail()});
+            final String content=String.format(pro.getProperty("mail.kill.item.success.content"),info.getItemName(),info.getCode());
+            mailDto.setSubject(pro.getProperty("mail.kill.item.success.subject"));
+            mailDto.setContent(content);
+            //调用发送邮件服务
+            mailService.sendHtmlMail(mailDto);
             System.out.println("接受消息为："+info);
         }catch (Exception e){
             System.out.println("接受消息出现异常"+e);
---
+++ killmall/src/main/java/seu/hy/killmall/service/MailService.java
@@ -0,0 +1,95 @@
+package seu.hy.killmall.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import seu.hy.killmall.dto.MailDto;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+@Service
+public class MailService {
+    private  static Properties pro;
+    static {
+        String filename="application.properties";
+        pro= new Properties();
+        try {
+            pro.load(new InputStreamReader(MailService.class.getClassLoader().getResourceAsStream(filename),"utf-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    /**
+     * 发送简单文本邮件
+     * */
+    @Async
+    public void sendSimpleMail(final MailDto mailDto){
+        try{
+            SimpleMailMessage message =new SimpleMailMessage();
+            message.setFrom(pro.getProperty("mail.send.from"));
+            message.setSubject(mailDto.getSubject());
+            message.setTo(mailDto.getTos());
+            message.setText(mailDto.getContent());
+            mailSender.send(message);
+            System.out.println("发送简单文本文件成功");
+        }catch (Exception e){
+            System.out.println("发送简单文本邮件失败"+e);
+        }
+    }
+
+    /**
+     * 发送带html格式的邮件
+     * */
+
+    @Async
+    public void sendHtmlMail(MailDto mailDto){
+
+        MimeMessage message = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper mimeMessageHelper=new MimeMessageHelper(message,true,"utf-8");
+            mimeMessageHelper.setFrom(pro.getProperty("mail.send.from"));
+            mimeMessageHelper.setTo(mailDto.getTos());
+            mimeMessageHelper.setText(mailDto.getContent(),true);
+            mimeMessageHelper.setSubject(mailDto.getSubject());
+            mailSender.send(message);
+            System.out.println("邮件发送成功");
+        } catch (MessagingException e) {
+            System.out.println("邮件发送失败"+e);
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
